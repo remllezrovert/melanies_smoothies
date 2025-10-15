29d513d81e3e55f0977c0c7ec2059284b7794840
remllezrovert <86428833+remllezrovert@users.noreply.github.com>
2025-10-15 12:00:07 -0500

    Display smoothiefroot info as JSON in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from snowflake.snowpark.functions import col
 #New section to display smoothiefroot info
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 
 # Write directly to the app
